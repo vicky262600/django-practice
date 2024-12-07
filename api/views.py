c4a7d33fbaac7fa05629852886a024a16b17df30
Vikas Joshi <145147066+vicky262600@users.noreply.github.com>
2024-12-07 17:17:55 -0500

    all done

--- api/views.py
+++ api/views.py
@@ -1,10 +1,10 @@
+from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserSerializer, NoteSerializer
-from .models import Note  
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
+
 
 class NoteListCreate(generics.ListCreateAPIView):
     serializer_class = NoteSerializer
@@ -13,12 +13,13 @@ class NoteListCreate(generics.ListCreateAPIView):
     def get_queryset(self):
         user = self.request.user
         return Note.objects.filter(author=user)
-    
+
     def perform_create(self, serializer):
         if serializer.is_valid():
-            serializer.save(auther=self.request.user)
+            serializer.save(author=self.request.user)
         else:
             print(serializer.errors)
+
 
 class NoteDelete(generics.DestroyAPIView):
     serializer_class = NoteSerializer
@@ -26,26 +27,10 @@ class NoteDelete(generics.DestroyAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        return Note.objects.filter(auther=user)
+        return Note.objects.filter(author=user)
 
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
-
-    def create(self, request, *args, **kwargs):
-        # Call the default create method to save the user
-        response = super().create(request, *args, **kwargs)
-        user = User.objects.get(username=request.data['username'])
-        
-        # Generate JWT tokens
-        refresh = RefreshToken.for_user(user)
-        access_token = str(refresh.access_token)
-        refresh_token = str(refresh)
-        
-        # Include tokens in the response
-        response.data['accessToken'] = access_token
-        response.data['refreshToken'] = refresh_token
-        
-        return response
